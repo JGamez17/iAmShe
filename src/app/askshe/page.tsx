603f0e97feb77ce89402b989d805f20d4fecf9b8
JGamez17 <github email address>
2024-04-12 15:09:24 -0400

    Fixed git issues with AskShe folder

--- src/app/askshe/page.tsx
+++ src/app/askshe/page.tsx
@@ -1,2 +1,76 @@
-"Hello World"
+'use client'
+import React, { FormEvent, useState, } from 'react';
 
+const AskShe = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const onSubmit = async (e: FormEvent) => {
+        e.preventDefault();
+        try {
+            const res = await fetch('https://api.web3forms.com/submit', {
+                method: 'POST',
+                body: JSON.stringify({
+                    access_key: "d38f8668-f2a0-4d7d-90d4-f7350c4f9abc",
+                    name,
+                    email,
+                    message,
+                }),
+                headers: {
+                    'content-type': 'application/json',
+                },
+            });
+            if (res.ok) {
+                const result = await res.json();
+                console.log(result);
+            } else {
+                console.error('Error:', res.statusText);
+            }
+        } catch (err) {
+            console.error('Error:', err);
+        }
+    };
+
+    return (
+        <div className="bg-black min-h-screen flex flex-col items-center justify-center">
+            <h3 className="text-3xl md:text-4xl font-bold text-white mt-20 mb-8">
+                Ask She - Questions, Comments, Concerns, please submit below
+            </h3>
+            <form onSubmit={onSubmit} className="form-container flex flex-col gap-8">
+                <div className="form-group">
+                    <label htmlFor="name">Name: </label>
+                    <input
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        type="text"
+                        id="name"
+                        placeholder="Enter your name"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email: </label>
+                    <input
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        type="email"
+                        id="email"
+                        placeholder="name@example.com"
+                    />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Message: </label>
+                    <textarea
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        id="message"
+                        placeholder="Enter your message"
+                    ></textarea>
+                </div>
+                <button className="btn btn-primary" type="submit">Submit</button>
+            </form>
+        </div >
+    );
+};
+
+export default AskShe;
